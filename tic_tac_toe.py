469a4d57a6964ff94f23eca63a581ce07534a2b3
carlieb <ball.carlie@gmail.com>
2015-09-18 21:01:52 -0700

    added winner function

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -30,5 +30,22 @@ def is_square_occupied(num):
   if squares[num] == ' ':
     return squares[num] != ' '
 
-print(record_moves())
-create_board(squares)
+def squares_are_full():
+  for index in range(1, 10):
+    #have to use index in range in order to avoid testing 0, as there is no box assigned to number 0, the loop will always find 0 empty and not test boxes 1-9 which are the real boxes
+    if squares[index] == ' ':
+      return False
+  return True
+
+def three_in_a_row():
+  return((squares[1] == squares[2] == squares[3] and squares[1] != ' ') or
+    (squares[4] == squares[5] == squares[6] and squares [4] != ' ') or
+    (squares[7] == squares[8] == squares[9] and squares [7] != ' ') or
+    (squares[1] == squares[4] == squares[7] and squares [1] != ' ') or
+    (squares[2] == squares[5] == squares[8] and squares [2] != ' ') or
+    (squares[3] == squares[6] == squares[9] and squares [3] != ' ') or
+    (squares[1] == squares[5] == squares[9] and squares [1] != ' ') or
+    (squares[3] == squares[5] == squares[7] and squares [3] != ' '))
+
+# print(record_moves())
+# create_board(squares)
